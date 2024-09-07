f61cc976c9f59de8beeff4324121a2fa8766f972
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-07 16:36:47 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Rohit Nikam on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
