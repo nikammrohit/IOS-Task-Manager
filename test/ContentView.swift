ee1c32ed118e85872312e633d356f5108f75f41d
nikammrohit <82987914+nikammrohit@users.noreply.github.com>
2024-09-08 12:51:08 -0500

    created and developed an app that will add tasks based on user specified date

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -6,16 +6,86 @@
 //
 
 import SwiftUI
+import GoogleGenerativeAI
+
+struct Task: Identifiable{
+    let id = UUID()
+    var name: String
+    var date: Date
+}
 
 struct ContentView: View {
+    @State private var taskName: String = ""
+    @State private var tasks: [Task] = []
+    @State private var selectedDate: Date = Date()
+
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            
+            Spacer()
+            Circle()
+                .fill(.blue)
+                .frame(width: 200)
+                .overlay(
+                    Image(systemName: "calendar.circle")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+            
+            Spacer()
+                .frame(height: 30)
+            
+            DatePicker(selection: $selectedDate,
+                       label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
+                .labelsHidden()
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            TextField("Task Name", text: $taskName)
+                .multilineTextAlignment(.center)
+            
+            Button("Add") {
+                if !taskName.isEmpty{
+                    let newTask = Task(name: taskName, date: selectedDate)
+                    tasks.append(newTask)
+                    taskName = ""
+                }
+            }
+            
+            
+            
+            List{
+                
+                ForEach (tasks.indices, id: \.self){index in
+                    HStack{
+                        
+                        Text(tasks[index].name)
+                        Spacer()
+                        Text(tasks[index].date, style: .date)
+                        
+                        Spacer()
+                        
+                        Button (action: {
+                            deleteTask(at: index)
+                        }) {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                                
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    private func deleteTask(at index: Int){
+        tasks.remove(at: index)
     }
 }
 
